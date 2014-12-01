cf22cd22b992a302b34f83cc99b17fae3f5be29b
Caroline Wong <carowong5@gmail.com>
2014-12-01 14:28:50 -0800

    hacker news loads into div

--- feed.js
+++ feed.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function() {
 	$(".feed").draggable({
 		grid: [20,20]
@@ -5,5 +7,25 @@ $(document).ready(function() {
 	.resizable({
 		grid: 30
 	});
+
+	
 });
+
+
+// 	var initialize = function(url) {
+// 		console.log('asdf');
+// 		var feed = new google.feeds.Feed("https://news.ycombinator.com/rss");
+// 		feed.load(function(result) {
+// 			if (!result.error) {
+// 				var container = document.getElementById("hn");
+// 				for (var i = 0; i < result.feed.entries.length; i++) {
+// 					var entry = result.feed.entries[i];
+// 					var div = document.createElement("div");
+// 					div.appendChild(document.createTextNode(entry.title));
+// 					container.appendChild(div);
+// 				}
+// 			}
+// 	  });
+// 	}
+// google.setOnLoadCallback(initialize);
 
