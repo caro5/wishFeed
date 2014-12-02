89be6b8b5db0171b133b2d554782bef47f6e4ec3
Caroline Wong <carowong5@gmail.com>
2014-12-02 15:52:06 -0800

    added more styling: buttons and background

--- feed.js
+++ feed.js
@@ -1,3 +1,5 @@
+var fb = new Firebase("https://amber-torch-8493.firebaseio.com/");
+var feeds = fb.child("allFeeds");
 var urls = {
 	"hackerNews": "https://news.ycombinator.com/bigrss",
 	"dribbble":"https://dribbble.com/shots/popular.rss",
@@ -7,9 +9,38 @@ var urls = {
 	'reddit':'http://www.reddit.com/.rss'
 };
 
+//store feedNames already used
 var alreadyUsed = {};
-$(document).ready(function() {
 
+//populates feed from fb
+var getFeeds = function() {
+	fb.once('value', function(snapshot) {
+		var objArr = snapshot.child('allFeeds').val();
+		for (var k in objArr) {
+			var obj = objArr[k];
+				alreadyUsed[obj.name] = true;
+			$('#allFeeds')
+			.append('<div class="feed" id="'+ obj.name + '" style="top: '+obj.top +'px; left:'+obj.left+'px; height:'+obj.height+'px;width:'+obj.width+'px;"></div>');
+			$('#'+obj.name).draggable({
+			grid: [20,20]		
+			})
+			.resizable({
+				grid: 30
+			});	
+			var feedUrl = urls[obj.name];
+			if (obj.name === 'dribbble') {
+				getDribbble();
+			} else {
+				parseFeed(feedUrl, obj.name);
+			}
+		}
+	});
+};
+
+
+$(document).ready(function() {
+	//if firebase has divs populate
+	getFeeds();
 	$('#optionsList').hide();
 	$('#addFeed').hide();
 
@@ -50,20 +81,56 @@ var getDribbble = function() {
 			html.push('<img src="' + shot.image_teaser_url + '" ');
 			html.push('</a>')
 			var fullString = html.join('');
-			console.log(fullString);
 			// console.log(shot);
 		});
 		$(str).append('<p>dribbble</p>').append(html.join(''));
 	}, {page: 2, per_page: 10});
 };
 
+// saveButton clicked -> saves to firebase the divs and their dimensions
+var saveFeeds = function() {
+	fb.child('allFeeds').remove();
+	// fb.once('value', function(snapshot) {
+	// 	var objArr = snapshot.child('allFeeds').val();
+	// 	for (var k in objArr) {
+	// 		var obj = objArr[k];
+	// 		// console.log(snapshot.child('allFeeds').val());	
+	// 		$('#allFeeds')
+	// 		.append('<div class="feed" id="'+ obj.name + '" style="top: '+obj.top +'; left:'+obj.left+'px; height:'+obj.height+'px;width:'+obj.width+'px;"></div>');
+	// 		$('#'+obj.name).draggable({
+	// 		grid: [20,20]		
+	// 		})
+	// 		.resizable({
+	// 			grid: 30
+	// 		});	
+			
+	// 	}
+	// });
+
+	$("#allFeeds > div").each(function() {
+		var id = $(this).attr('id');
+		var str = "#" + id;
+
+		var objectToSave = {
+			'name': id,
+			'width': $(str).width(),
+			'height': $(str).height(),
+			'top': $(str).position().top,
+			'left': $(str).position().left
+		};
+		fb.child('allFeeds').push(objectToSave);
+	});
+
+};
 var addFeedToPage = function() {
 	// console.log($('#optionsList').find('li.ui-selected'));
 	var conceptName = $('#optionsList').find('li.ui-selected')[0].id;
 	var idName = conceptName.slice(0,conceptName.length - 6);
 	if (alreadyUsed[idName]) {
+		console.log('already exists');
 		alert('feed already exists');
 	} else {
+		console.log('doesnt');
 		alreadyUsed[idName] = true;
 		$('#allFeeds').append('<div class="feed" id="'+idName + '"></div>');
 		
