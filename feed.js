c9ff64b662fc03ebb92a8fa42a83af99e331997a
Caroline Wong <carowong5@gmail.com>
2014-12-01 22:05:55 -0800

    add feeds by clicking on an option and pressing the add feed button

--- feed.js
+++ feed.js
@@ -1,3 +1,13 @@
+var urls = {
+	"hackerNews": "https://news.ycombinator.com/bigrss",
+	"dribbble":"https://dribbble.com/shots/popular.rss",
+	"quora":"http://www.quora.com/rss",
+	 "bbc": "http://feeds.bbci.co.uk/news/rss.xml",
+	'scienceDaily':'http://feeds.sciencedaily.com/sciencedaily/top_news?format=xml',
+	'reddit':'http://www.reddit.com/.rss'
+};
+
+var alreadyUsed = {};
 $(document).ready(function() {
 	$(".feed")
 	.draggable({
@@ -6,40 +16,66 @@ $(document).ready(function() {
 	.resizable({
 		grid: 30
 	});	
+
+	$('#optionsList').selectable({
+		selected: function(event, ui) {
+		console.log(ui);
+		},
+		unselecting: function( event, ui ) {
+			console.log(ui);
+			ui.removeAttr('ui-selected');
+		}
+	});	
 });
 
-$(function() {
-	$('#optionsList').selectable();
-});
 var addFeedToPage = function() {
-	var feedContainer = document.getElementById('allFeeds');
-	console.log(feedContainer);
-	var div = document.createElement("div");
-	div.addClass('feed');
-	div.text = "asdf";
-	feedContainer.appendChild(div);
+	// console.log($('#optionsList').find('li.ui-selected'));
+	var conceptName = $('#optionsList').find('li.ui-selected')[0].id;
+	var idName = conceptName.slice(0,conceptName.length - 6);
+	console.log(alreadyUsed);
+	if (alreadyUsed[idName]) {
+		alert('feed already exists');
+	} else {
+		alreadyUsed[idName] = true;
+		
+		$('#allFeeds').append('<div class="feed" id="'+idName + '"></div>');
+		
+		$('.feed')
+		.draggable({
+			grid: [20,20]		
+		})
+		.resizable({
+			grid: 30
+		});	
 
+		var feedUrl = urls[idName];
+		parseFeed(feedUrl, idName);
+	}	
 };
-// var script = document.createElement('script');
-// script.setAttribute('src', "https://www.google.com/jsapi");
-// $("head").append(script);
 
-// google.load("feeds", "1");
-// var parseFeed = function(url, cont) {
-// 	var feed = new google.feeds.Feed(url);
-// 	feed.setNumEntries(20); //need to set or else defaults to 4
-// 	feed.load(function(result) {
-// 		if (!result.error) {
-// 			var container = document.getElementById(cont);
-// 			console.log(result);
-// 			for (var i = 0; i < result.feed.entries.length; i++) {
-// 				var entry = result.feed.entries[i];
-// 				var div = document.createElement("div");
-// 				div.appendChild(document.createTextNode(entry.title));
-// 				container.appendChild(div);
-// 			}
-// 		}
-//   });
-// }
-// google.setOnLoadCallback(initialize);
-// 	
+
+// parses feeds and populates containers
+google.load("feeds", "1");
+var parseFeed = function(url, cont) {
+	var feed = new google.feeds.Feed(url);
+	feed.setNumEntries(30); //need to set or else defaults to 4
+	feed.load(function(result) {
+		if (!result.error) {
+			console.log(cont);
+			var str = "#" + cont;
+			var div = document.createElement("div");
+				div.appendChild(document.createTextNode(cont));
+			for (var i = 0; i < result.feed.entries.length; i++) {
+				var entry = result.feed.entries[i];
+				var a = document.createElement("a");
+				var img = document.createElement("img");
+				img = entry.content;
+				a.text = entry.title;
+				a.href = entry.link;
+				div.appendChild(a);
+				$(str).append(div);
+			}
+		}
+  });
+};
+google.setOnLoadCallback(parseFeed);
